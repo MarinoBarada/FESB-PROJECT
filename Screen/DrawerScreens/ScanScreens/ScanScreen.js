c15028da6bfdba4588034e71ae6f7cc8201bb306
JakovJonjić <73243731+Jakov69@users.noreply.github.com>
2024-02-03 23:05:24 +0100

    I make, Scan, PopUp, PresenceScreen, PresenceSubjectScreeen

--- Screen/DrawerScreens/ScanScreens/ScanScreen.js
+++ Screen/DrawerScreens/ScanScreens/ScanScreen.js
@@ -1,7 +1,15 @@
-import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { View, Text, SafeAreaView, Image, TouchableOpacity } from "react-native";
+import PopUpComponent from '../../Components/PopUp';
 
 const ScanScreen = () => {
+
+  const [isPopupVisible, setPopupVisible] = useState(false);
+
+  const togglePopup = () => {
+    setPopupVisible(!isPopupVisible);
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1, padding: 16 }}>
@@ -18,9 +26,38 @@ const ScanScreen = () => {
               textAlign: "center",
               marginBottom: 16,
             }}
+            onPress={togglePopup}
           >
-            Ovo je Scan Screen
+            <View style={{ alignItems: "center" }}>
+              <Image
+                source={require("../../../Image/CircleNFC.png")}
+                style={{
+                  width: 240,
+                  height: 300,
+                  resizeMode: "contain",                  
+                }}
+              />
+              <Text
+              style={{
+                fontSize: 24,
+                textAlign: "center",
+                marginBottom: 16,
+                fontWeight: 700,
+               }}
+              >SKENIRAJ</Text>
+              <Text
+              style={{
+                fontSize: 15,
+                textAlign: "center",
+                marginBottom: 16,
+                fontWeight: 400,
+               }}
+              >Pritisnite i približite svoj uređaj NFC-u</Text>
+            </View>
+            
+
           </Text>
+          <PopUpComponent isVisible={isPopupVisible} onClose={togglePopup} />
         </View>
       </View>
     </SafeAreaView>
